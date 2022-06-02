77930152e880dc11726906829aa412bfbc504214
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-01 20:40:09 -0500

    MovieModel 3

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo5_movieapp/models/movie_model.dart';
+import 'package:flutter_codigo5_movieapp/ui/widgets/item_movie_list_widget.dart';
 import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
@@ -32,7 +33,8 @@ class _HomePageState extends State<HomePage> {
       // movies.forEach((element) {
       //   moviesList.add(MovieModel.fromJson(element));
       // });
-      moviesList = movies.map<MovieModel>((e) => MovieModel.fromJson(e)).toList();
+      moviesList =
+          movies.map<MovieModel>((e) => MovieModel.fromJson(e)).toList();
 
       setState(() {});
     }
@@ -40,8 +42,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-
     return Scaffold(
       backgroundColor: Color(0xff161823),
       body: SafeArea(
@@ -50,150 +50,43 @@ class _HomePageState extends State<HomePage> {
             padding:
                 const EdgeInsets.symmetric(horizontal: 14.0, vertical: 12.0),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                const Text(
+                  "Discover",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.redAccent,
+                        ),
+                      ),
+                      child: Text(
+                        "Action",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
                 ListView.builder(
                   shrinkWrap: true,
                   physics: ScrollPhysics(),
                   itemCount: moviesList.length,
                   itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      margin: const EdgeInsets.symmetric(
-                          vertical: 16.0, horizontal: 14.0),
-                      width: double.infinity,
-                      height: height * 0.65,
-                      decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.3),
-                            offset: const Offset(4, 4),
-                            blurRadius: 12.0,
-                          ),
-                        ],
-                        borderRadius: BorderRadius.circular(22.0),
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: NetworkImage(
-                            "http://image.tmdb.org/t/p/w500${movies[index]["poster_path"]}",
-                          ),
-                        ),
-                      ),
-                      child: Stack(
-                        children: [
-                          Align(
-                            alignment: Alignment.bottomCenter,
-                            child: Container(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 12.0, vertical: 12.0),
-                              decoration: BoxDecoration(
-                                color: Colors.black.withOpacity(0.8),
-                                borderRadius: const BorderRadius.only(
-                                  bottomRight: Radius.circular(22),
-                                  bottomLeft: Radius.circular(22),
-                                ),
-                              ),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Text(
-                                    // movies[index]["original_title"],
-                                    moviesList[index].originalTitle,
-                                    maxLines: 2,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: const TextStyle(
-                                      fontSize: 20.0,
-                                      fontWeight: FontWeight.w700,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    height: 8.0,
-                                  ),
-                                  Text(
-                                    // movies[index]["overview"],
-                                    moviesList[index].overview,
-                                    maxLines: 3,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(
-                                      color: Colors.white.withOpacity(0.8),
-                                      fontSize: 13.0,
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    height: 6.0,
-                                  ),
-                                  Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Row(
-                                        children: [
-                                          Icon(
-                                            Icons.date_range,
-                                            color: Colors.white,
-                                            size: 14.0,
-                                          ),
-                                          const SizedBox(
-                                            width: 4.0,
-                                          ),
-                                          Text(
-                                            // movies[index]["release_date"],
-                                            moviesList[index].releaseDate.toString().substring(0, 10),
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 12.0),
-                                          ),
-                                        ],
-                                      ),
-                                      Row(
-                                        children: [
-                                          Icon(
-                                            Icons.thumb_up_alt,
-                                            color: Colors.white,
-                                            size: 14.0,
-                                          ),
-                                          const SizedBox(
-                                            width: 4.0,
-                                          ),
-                                          Text(
-                                            // movies[index]["vote_count"].toString(),
-                                            moviesList[index].voteCount.toString(),
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 12.0),
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                  const SizedBox(
-                                    height: 6.0,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          Align(
-                            alignment: Alignment.topRight,
-                            child: Container(
-                              padding: const EdgeInsets.all(14.0),
-                              margin: const EdgeInsets.all(12.0),
-                              decoration: BoxDecoration(
-                                color: Colors.black.withOpacity(0.8),
-                                shape: BoxShape.circle,
-                              ),
-                              child: Text(
-                                moviesList[index].voteAverage.toString(),
-                                style: TextStyle(
-                                  fontSize: 20.0,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+                    return ItemMovieListWidget(
+                      movieModel: moviesList[index],
                     );
                   },
                 ),
