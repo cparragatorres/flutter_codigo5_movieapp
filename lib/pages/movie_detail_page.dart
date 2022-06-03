f88f7fb77e5e95b4bda0e84c9d815aff8803f03e
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-02 19:30:27 -0500

    MovieDetailPage 2

--- lib/pages/movie_detail_page.dart
+++ lib/pages/movie_detail_page.dart
@@ -7,7 +7,48 @@ class MovieDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text("Hola"),
+            centerTitle: true,
+            expandedHeight: 200.0,
+            flexibleSpace: FlexibleSpaceBar(),
+          ),
 
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Container(
+                  height: 200.0,
+                  color: Colors.redAccent,
+                ),
+                Container(
+                  height: 200.0,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  height: 200.0,
+                  color: Colors.green,
+                ),
+                Container(
+                  height: 200.0,
+                  color: Colors.redAccent,
+                ),
+                Container(
+                  height: 200.0,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  height: 200.0,
+                  color: Colors.green,
+                ),
+              ],
+            ),
+          ),
+
+        ],
+      ),
     );
   }
 }
