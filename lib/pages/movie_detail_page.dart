3d5ca252c9be731f8f226775203fa7906f0ff2d9
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-02 22:29:34 -0500

    HomePage 7

--- lib/pages/movie_detail_page.dart
+++ lib/pages/movie_detail_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo5_movieapp/ui/general/colors.dart';
 import 'package:flutter_codigo5_movieapp/ui/widgets/item_cast_widget.dart';
+import 'package:flutter_codigo5_movieapp/ui/widgets/item_review_widget.dart';
 import 'package:flutter_codigo5_movieapp/ui/widgets/title_description_widget.dart';
 
 class MovieDetailPage extends StatelessWidget {
@@ -257,29 +258,13 @@ class MovieDetailPage extends StatelessWidget {
                       const SizedBox(
                         height: 10.0,
                       ),
-                      Container(
-                        // color: Colors.white54,
-                        child: ExpansionTile(
-                          textColor: Colors.white,
-                          iconColor: Colors.white,
-                          collapsedIconColor: Colors.white,
-                          childrenPadding: const EdgeInsets.symmetric(horizontal: 16.0),
-                          title: Text(
-                            "Review 1",
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                          children: [
-                            Text(
-                              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                              style: TextStyle(
-                                color: Colors.white70,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
+
+                      ItemReviewWidget(),
+                      ItemReviewWidget(),
+                      ItemReviewWidget(),
+                      ItemReviewWidget(),
+                      ItemReviewWidget(),
+
                       const SizedBox(
                         height: 200,
                       ),
