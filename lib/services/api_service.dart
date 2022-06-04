aca117dfde2ee03bfe89e7739c6fe4583c8189d5
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-03 22:13:53 -0500

    Cast 7

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter_codigo5_movieapp/models/cast_model.dart';
 import 'package:flutter_codigo5_movieapp/models/movie_detail_model.dart';
 import 'package:flutter_codigo5_movieapp/models/movie_model.dart';
+import 'package:flutter_codigo5_movieapp/models/review_model.dart';
 import 'package:flutter_codigo5_movieapp/pages/movie_detail_page.dart';
 import 'package:flutter_codigo5_movieapp/utils/constants.dart';
 import 'package:http/http.dart' as http;
@@ -49,15 +50,17 @@ class APIService{
     return [];
   }
 
-  getReviews(int movieId) async{
+  Future<List<ReviewModel>> getReviews(int movieId) async{
     String path = "$pathProduction/movie/$movieId/reviews?api_key=$apiKey&language=en-US&page=1";
     Uri _uri = Uri.parse(path);
     http.Response response = await http.get(_uri);
     if(response.statusCode == 200){
       Map<String, dynamic> myMap = json.decode(response.body);
       List results = myMap["results"];
-      print(results);
+      List<ReviewModel> reviews = results.map((e) => ReviewModel.fromJson(e)).toList();
+      return reviews;
     }
+    return [];
   }
 
 
